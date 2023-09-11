234ad00a1b8591997e6618a8a0b366385c4d3860
Matheus Morillo <matheus.morillo2000@gmail.com>
2023-09-11 16:31:40 -0300

    add recursive version

--- src/main.rs
+++ src/main.rs
@@ -28,13 +28,20 @@ fn fibonacci(n: u32) -> u32 {
 
     if n == 1 || n == 2 { return 1; }
 
-    for _ in 0..n-2 {
+    for _ in 0..n - 2 {
         let aux = curr;
         curr = curr + prev;
         prev = aux;
     }
 
     curr
+}
+
+
+fn fibonacci_recursive(n: u32) -> u32 {
+    if n == 1 || n == 2 { return 1; }
+
+    return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2);
 }
 
 #[cfg(test)]
@@ -44,15 +51,18 @@ mod tests {
     #[test]
     fn should_return_the_fibonacci_of_a_given_number() {
         assert_eq!(fibonacci(6), 8);
+        assert_eq!(fibonacci_recursive(6), 8);
     }
 
     #[test]
     fn should_return_the_fibonacci_of_2() {
         assert_eq!(fibonacci(2), 1);
+        assert_eq!(fibonacci_recursive(2), 1);
     }
 
     #[test]
     fn should_return_the_fibonacci_of_1() {
         assert_eq!(fibonacci(1), 1);
+        assert_eq!(fibonacci_recursive(1), 1);
     }
 }
